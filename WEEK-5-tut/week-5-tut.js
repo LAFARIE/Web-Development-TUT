3795b81eaa55a804f5674d331965eb7b99b0cf35
dillon-sj <ds.juriansz@gmail.com>
2023-03-15 14:01:24 +0530

    2.01

--- WEEK-5-tut/week-5-tut.js
+++ WEEK-5-tut/week-5-tut.js
@@ -2,11 +2,7 @@ function randomNum(num1,num2){
 for(var i=0;i<10;i++){
 var num1 = Math.floor(Math.random() * 10);      
 var num2 = Math.floor(Math.random() * 10);
-
+return num1+" + "+num2+"="
 }
 }
 document.getElementById("question1").innerHTML = randomNum();  
-document.getElementById("question2  ").innerHTML = randomNum();  
-
-
-
